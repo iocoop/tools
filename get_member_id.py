08f6b9ad53073cb0a7f37d4262d37525fc136b04
Gene Wood <gene_wood@cementhorizon.com>
2013-06-09 08:57:22 -0700

    adding a list all member mode

--- get_member_id.py
+++ get_member_id.py
@@ -4,8 +4,10 @@ import argparse
 import pprint
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
 parser = argparse.ArgumentParser(description='Determine a freshbooks client_id from a client email address')
-parser.add_argument('email_address', nargs='+',
+parser.add_argument('email_address', nargs='*',
                    help='email address to search for')
 parser.add_argument('--config', '-c', dest='config', default='freshbooks.cfg',
                    help='config file name (default: freshbooks.cfg)')
@@ -29,12 +31,17 @@ for email_address in args.email_address:
     
 client_response = c.client.list()
 logging.debug('total pages : %s' % client_response.clients.attrib['pages'])
-for page in range(1, int(client_response.clients.attrib['pages']) + 2):
+for page in range(2, int(client_response.clients.attrib['pages']) + 2):
     for client in client_response.clients.client:
         logging.debug('client.email = %s' % client.email)
-        for email_address in args.email_address:
-            if str(client.email).lower() == email_address.lower():
-                client_ids[email_address][client.first_name + ' ' + client.last_name + ' : ' + client.organization] = client.client_id
+        if len(client_ids) > 0:
+            for email_address in args.email_address:
+                if str(client.email).lower() == email_address.lower():
+                    client_ids[email_address][client.first_name + ' ' + client.last_name + ' : ' + client.organization] = client.client_id
+        else:
+            # logging.info("%s '%s' '%s %s'" % (str(client.email).lower(), client.organization, client.first_name, client.last_name))
+            logging.info(str(client.email).lower())
+
     logging.debug('page %s' % page)
     client_response = c.client.list(page=page)
 pprint.pprint(client_ids)
