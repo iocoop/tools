96a997d7458694f85a52209b79afac10e9cd9779
Gene Wood <gene_wood@cementhorizon.com>
2015-01-13 13:06:05 -0800

    Adding a tool to list out all active recurring profiles

---
+++ get_active_recurring_profiles.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from refreshbooks import api
+import ConfigParser
+import argparse
+import pprint
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+parser = argparse.ArgumentParser(description='Get current not-stopped invoice recurring profiles')
+parser.add_argument('--config', '-c', dest='config', default='/etc/freshbooks.cfg',
+                   help='config file name (default: /etc/freshbooks.cfg)')
+
+args = parser.parse_args()
+
+config = ConfigParser.RawConfigParser()
+config.read(args.config)
+
+api_token = config.get('Freshbooks', 'api_token')
+
+c = api.TokenClient(
+    'iocoop.freshbooks.com',
+    api_token,
+    user_agent='get_active_recurring_profiles.py/1.0'
+)
+
+clients = {}
+client_response = c.client.list()
+logging.debug('total pages : %s' % client_response.clients.attrib['pages'])
+for page in range(2, int(client_response.clients.attrib['pages']) + 2):
+    for client in client_response.clients.client:
+        logging.debug('client.email = %s' % client.email)
+        clients[client.client_id] = client
+    logging.debug('page %s' % page)
+    client_response = c.client.list(page=page)
+
+recurring_response = c.recurring.list()
+logging.debug('total pages : %s' % recurring_response.recurrings.attrib['pages'])
+for page in range(2, int(recurring_response.recurrings.attrib['pages']) + 2):
+    for recurring in recurring_response.recurrings.recurring:
+        logging.debug('recurring.recurring_id = %s' % recurring.recurring_id)
+        logging.debug('recurring.stopped = %s' % recurring.stopped)
+        if recurring.stopped == 0:
+            #logging.info("%s: %s '%s' '%s %s'" % (recurring.recurring_id, , clients[recurring.client_id], recurring.organization, recurring.first_name, recurring.last_name))
+            #logging.info("%s" % clients[recurring.client_id].email)
+            print '"%s", "%s", "%s %s", "%s"' % (recurring.recurring_id, str(clients[recurring.client_id].email).lower(), clients[recurring.client_id].first_name, clients[recurring.client_id].last_name, clients[recurring.client_id].organization)
+    logging.debug('page %s' % page)
+    recurring_response = c.recurring.list(page=page)
+#pprint.pprint(client_ids)
